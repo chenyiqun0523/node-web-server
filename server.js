d25b0cf6dfb2a7dde9a39b8947515050137926d0
JeffC <chenyiqun0523@gmail.com>
2018-05-16 21:56:08 -0700

    add project page

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-// app.use((req, res, next) => {
-//   var now = new Date().toString();
-//   var log = `${now}: ${req.method} ${req.url}`;
-//
-//   console.log(log);
-//   fs.appendFile('server.log', log + '\n');
-//   next();
-// });
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log(`Unable to write to file`);
+    }
+  });
+  next();
+});
+
+ // app.use((req, res, next) => {
+ //   res.render('maintenance.hbs');
+ // });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -36,6 +40,12 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
